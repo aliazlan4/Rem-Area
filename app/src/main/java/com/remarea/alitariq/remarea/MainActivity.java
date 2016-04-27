3f4292fbabca46c359a3dca335287968d15dbd94
13beseatariq <13beseatariq@seecs.edu.pk>
2016-04-27 16:13:20 +0500

    Login Screen

--- app/src/main/java/com/remarea/alitariq/remarea/MainActivity.java
+++ app/src/main/java/com/remarea/alitariq/remarea/MainActivity.java
@@ -1,13 +1,23 @@
 package com.remarea.alitariq.remarea;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        View v = findViewById(R.id.login);
+        View root = v.getRootView();
+        root.setBackgroundColor(getResources().getColor(android.R.color.white));
     }
 }
