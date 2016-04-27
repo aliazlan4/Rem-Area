cd98777e3bc91750d61a3a015104867530b5a851
13beseatariq <13beseatariq@seecs.edu.pk>
2016-04-27 16:20:20 +0500

    Login Screen

--- app/src/main/java/com/remarea/alitariq/remarea/MainActivity.java
+++ app/src/main/java/com/remarea/alitariq/remarea/MainActivity.java
@@ -1,13 +1,22 @@
 package com.remarea.alitariq.remarea;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    EditText username, password;
+    Button login;
+    TextView register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,26 @@ public class MainActivity extends Activity {
         View v = findViewById(R.id.login);
         View root = v.getRootView();
         root.setBackgroundColor(getResources().getColor(android.R.color.white));
-    }
+
+        username= (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
+    }//OnCreate method ends here
+
+
+
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v==findViewById(R.id.login))
+        {
+
+        }
+        else if (v==findViewById(R.id.register))
+        {
+
+        }
+    }//OnClick method ends here
 }
